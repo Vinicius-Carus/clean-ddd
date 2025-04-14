24e593b89f4e0d96045d789a3536e1789e909ac7
Vinicius Carus <viniciuscaruss@gmail.com>
2025-04-14 16:33:34 -0300

    feat: refactor entities to extend base Entity class and implement Slug value object

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,21 +1,11 @@
-import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
+import { Entity } from "../../core/entities/entity";
 
 interface QuestionProps {
   title: string;
   content: string;
+  slug: Slug;
   authorId: string;
 }
 
-export class Question {
-  public id: string;
-  public title: string;
-  public content: string;
-  public authorId: string;
-
-  constructor(props: QuestionProps, id?: string) {
-    this.id = id || randomUUID();
-    this.title = props.title;
-    this.content = props.content;
-    this.authorId = props.authorId;
-  }
-}
+export class Question extends Entity<QuestionProps> {}
