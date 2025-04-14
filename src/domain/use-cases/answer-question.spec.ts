ad21e5c501465d632d892b68430ba99fe0be93b9
Vinicius Carus <viniciuscaruss@gmail.com>
2025-04-14 17:15:33 -0300

    feat: update Answer and Question entities to use UniqueEntityID for ID management

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
 import { create } from "domain";
 import { Answer } from "../entities/answer";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 const fakeAnswersRepository = {
   create: async (answer: Answer) => {
@@ -14,8 +15,8 @@ test("create an answer", async () => {
 
   const answer = await answerQuestion.execute({
     content: "Answer content",
-    instructorId: "1",
-    questionId: "1",
+    instructorId: new UniqueEntityID("1"),
+    questionId: new UniqueEntityID("1"),
   });
 
   expect(answer.content).toEqual("Answer content");
